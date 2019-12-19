fa4aa07dec7df96385b0e63c77b17a970fdf009e
JPogodzinski <pogodzinskij@gmail.com>
2019-12-19 10:48:15 +0100

    works

--- shamir.py
+++ shamir.py
@@ -1,20 +1,45 @@
 from random import randrange
 
+
+def isPrime(n):
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+p = randrange(1000, 10000)
+while not isPrime(p):
+        p = randrange(1000, 10000)
+
+def sigma(a, x, t):
+    result = 0
+    for j in range(1, t - 1):
+        result += a[j] * (x ** j)
+    return result
+
+
+def getSecret(t, tab):
+    sigma = 0
+    for i in range(1, t):
+        multiplication = 1
+        for j in range(1, t):
+            if i != j:
+                multiplication *= (-1*j) / (i - j)
+        sigma += tab[i] * multiplication
+    return sigma%p
+
+
 if __name__ == '__main__':
-    n=5
-    t=n
-    tab=[]
-    k=1000
-    s=345
-    for i in range(0,n-1):
-        tab.append(randrange(0,k-1))
-    tab.append(s)
-    for i in range(0, n-1):
-        tab[n-1]-=tab[i]
-    tab[n-1]=tab[n-1]%k
-    print(tab)
-    x=0
-    for i in range(0, n):
-        x+=tab[i]
-    x=x%k
-    print(x)
+
+    s = randrange(0, p - 1)
+    n = 10
+    a = [s]
+    t = 6
+    for i in range(1, t):
+        a.append(randrange(0, 1000))
+    print(a)
+    sTab = [s]
+    for i in range(1, n):
+        sTab.append((s + sigma(a, i, t)) % p)
+    print(sTab)
+    print(getSecret(t, sTab))
